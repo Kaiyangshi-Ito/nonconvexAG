cd60975d347f5e2ede576e64c6cad562ba8ad489
Kai Yang <kai.yang2@mail.mcgill.ca>
2022-09-24 12:02:41 -0400

    add some scripts running on the server

---
+++ paper/simulation_study/tasks/task2speed/sub_tasks/task2speed_SCAD_coord_time/task2speed_SCAD_coord_time.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=def-bhatnaga
+#SBATCH --gres=gpu:1              # Number of GPU(s) per node
+#SBATCH --cpus-per-task=6         # CPU cores/threads
+#SBATCH --mem=185000M               # memory per node
+#SBATCH --time=6-12:00            # time (DD-HH:MM)
+#SBATCH --job-name=task2speed_SCAD_coord_time
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+./program
+
+module load gcc/9.3.0 r/4.0.2 python/3.8.10 python-build-bundle
+module use python
+module use python-build-bundle
+source $HOME/jupyter_py3/bin/activate
+pip install --no-index --upgrade pip
+pip install rpy2 matplotlib cupy numpy scipy
+
+python3 /home/kyang/tasks/task2speed_SCAD_coord_time/dist/task2speed_SCAD_coord_time.py
