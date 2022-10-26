be441154817336db86a486b8f6d009a7b8ae9fe6
Kai Yang <kai.yang2@mail.mcgill.ca>
2022-10-26 15:35:50 -0400

    add numerical plot

---
+++ paper/tighter_damping_lower_bound.m
@@ -0,0 +1,15 @@
+k1 = 2:1:2000;
+b1 = 0:1e-3:1;
+b1 = b1(2:length(b1)-1); % since b can't take 0 or 1
+[k, b] = meshgrid(k1, b1);
+Z = ((2./k).^b)./((1-b).*(4-b));
+plt = surfc(k,b,log(Z),log(Z));
+xlabel('k');
+ylabel('b');
+% zlabel('log(...)');
+hold on;
+plot3(k(log(Z)==min(log(Z))),b(log(Z)==min(log(Z))),min(log(Z)),"r");
+hold off;
+set(plt,"LineStyle","none");
+colorbar;
+view(-45,45);
