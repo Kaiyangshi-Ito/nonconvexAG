45cbca08be59ba228374a1d93cfca05b63258b7a
Kai Yang <kai.yang2@mail.mcgill.ca>
2022-10-27 17:19:55 -0400

    commit

--- paper/tighter_damping_lower_bound.m
+++ paper/tighter_damping_lower_bound.m
@@ -4,9 +4,9 @@ b1 = b1(2:length(b1)-1); % since b can't take 0 or 1
 [k, b] = meshgrid(k1, b1);
 Z = ((2./k).^b)./((1-b).*(4-b));
 plt = surfc(k,b,log(Z),log(Z));
-xlabel('k');
-ylabel('b');
-% zlabel('log(...)');
+xlabel('$k$','Fontsize',12, 'Interpreter','latex');
+ylabel('$b$','Fontsize',12, 'Interpreter','latex');
+zlabel('$\log(\bar{a}k^{-b})$','Fontsize',12, 'Interpreter','latex');
 hold on;
 plot3(k(log(Z)==min(log(Z))),b(log(Z)==min(log(Z))),min(log(Z)),"r");
 hold off;
