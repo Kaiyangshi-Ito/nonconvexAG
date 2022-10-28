3f3aa758fe0e676af941aac2150742ee64890dcf
Kai Yang <kai.yang2@mail.mcgill.ca>
2022-10-28 19:39:06 -0400

    commit

--- paper/tighter_lower_bound_plot.m
+++ paper/tighter_lower_bound_plot.m
@@ -3,7 +3,7 @@ k1 = 8:1:1e9;
 log_2overk = log(2) - log(k1);
 b1 = (2+5.*log_2overk+sqrt(9.*(log_2overk.^2)+4))/(2.*log_2overk);
 log_a1=b1.*log(2)-log(1-b1)-log(4-b1);
-log_obj=(log_a1+log(k1)).*(1-b1);
+log_obj=log_a1+log(k1).*(1-b1);
 plt=plot(k1,exp(log_obj)./log(k1));
 xlabel('$k$','Fontsize',20, 'Interpreter','latex');
 ylabel('$\frac{\bar{a}k^{1-\bar{b}}}{\log k}$','Fontsize',20, 'Interpreter','latex');
