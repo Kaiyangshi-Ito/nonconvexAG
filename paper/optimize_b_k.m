3d59cda4bad5160198faed581edfcea6ef8845f9
Kai Yang <kai.yang2@mail.mcgill.ca>
2022-11-01 13:26:35 -0400

    commit

--- paper/optimize_b_k.m
+++ paper/optimize_b_k.m
@@ -5,11 +5,11 @@ b1 = b1(2:length(b1)-1); % since b can't take 0 or 1
 [k, b] = meshgrid(k1, b1);
 log_Z = (b.*log(2./k))-log(1-b)-log(4-b);
 plt = surfc(k,b,log_Z,log_Z);
-xlabel('$k$','Fontsize',20, 'Interpreter','latex');
-ylabel('$b$','Fontsize',20, 'Interpreter','latex');
-zlabel('$\log(\bar{a}k^{-b})$','Fontsize',20, 'Interpreter','latex');
+xlabel('$k$','Fontsize',12, 'Interpreter','latex');
+ylabel('$b$','Fontsize',12, 'Interpreter','latex');
+zlabel('$\log(\bar{a}k^{-b})$','Fontsize',12, 'Interpreter','latex');
 hold on;
-plot3(k(log_Z==min(log_Z)),b(log_Z==min(log_Z)),min(log_Z),"r","LineWidth",1);
+plot3(k(log_Z==min(log_Z)),b(log_Z==min(log_Z)),min(log_Z),"r","LineWidth",1.5);
 hold off;
 set(plt,"LineStyle","none");
 colorbar;
