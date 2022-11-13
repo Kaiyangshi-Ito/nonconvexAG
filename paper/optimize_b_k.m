1d5d9454676b819314c74c63eef4673af8bf4c70
Kai Yang <kai.yang2@mail.mcgill.ca>
2022-11-13 09:32:40 -0500

    commit

--- paper/optimize_b_k.m
+++ paper/optimize_b_k.m
@@ -7,7 +7,7 @@ log_Z = (b.*log(2./k))-log(1-b)-log(4-b);
 plt = surfc(k,b,log_Z,log_Z);
 xlabel('$k$','Fontsize',12, 'Interpreter','latex');
 ylabel('$b$','Fontsize',12, 'Interpreter','latex');
-zlabel('$\log(\bar{a}k^{-b})$','Fontsize',12, 'Interpreter','latex');
+zlabel('$\log(\bar{a}_{k}k^{-b})$','Fontsize',12, 'Interpreter','latex');
 hold on;
 plot3(k(log_Z==min(log_Z)),b(log_Z==min(log_Z)),min(log_Z),"r","LineWidth",1.5);
 hold off;
--- paper/tighter_lower_bound_plot.m
+++ paper/tighter_lower_bound_plot.m
@@ -6,6 +6,6 @@ log_a1=b1.*log(2)-log(1-b1)-log(4-b1);
 log_obj=log_a1+log(k1).*(1-b1);
 plot(k1,exp(log_obj)./log(k1));
 xlabel('$k$','Fontsize',20, 'Interpreter','latex');
-ylabel('$\frac{\bar{a}k^{1-\bar{b}}}{\log k}$','Fontsize',20, 'Interpreter','latex');
+ylabel('$\frac{\bar{a}_{k}k^{1-\bar{b}_{k}}}{\log k}$','Fontsize',20, 'Interpreter','latex');
 plt=gca;
 exportgraphics(plt,'tighter_lower_bound_plot.eps','Colorspace','rgb','Resolution',600);
